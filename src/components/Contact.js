9a59cd36c222c27c915ee34d49e4b8860329703f
HSN-Studio <hassanrafique198@gmail.com>
2022-04-24 19:47:36 +0500

    field validtions on blur!

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,31 @@ import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 
 function Contact({ devices, step, handler }) {
   //States
-  const [ready, setReady] = useState(false);
+  const cities = [
+    "Arboretum",
+    "Cbd",
+    "Brookside Drive",
+    "Gigiri",
+    "Hurlingham",
+    "Kileleshwa",
+    "Kilimani",
+    "Kitisuru",
+    "Kyuna",
+    "Lavington",
+    "Loresho",
+    "Lower Kabete",
+    "Mountain View",
+    "Ngong Road (Up to Junction Mall)",
+    "Nyari",
+    "Parklands",
+    "Riverside Drive",
+    "Runda",
+    "Spring Valley",
+    "Upper hill",
+    "Westland",
+    "Other",
+  ];
+  const [ready, setReady] = useState(true);
   const [contactInfo, setcontactInfo] = useState({
     name: "",
     email: "",
@@ -22,10 +46,15 @@ function Contact({ devices, step, handler }) {
     deviceCollection: "Walk-In",
     time: "",
   });
+  const [nameError, setnameError] = useState(false);
+  const [emailError, setemailError] = useState(false);
+  const [mobileNumberError, setmobileNumberError] = useState(false);
+  const [addressError, setaddressError] = useState(false);
+  const [cityError, setcityError] = useState(false);
 
-  useEffect(() => {
-    console.table(devices);
-  }, []);
+  // useEffect(() => {
+  //   console.log(contactInfo);
+  // }, [contactInfo]);
 
   //Handler Functions
   const inputHandler = (event) => {
@@ -43,6 +72,41 @@ function Contact({ devices, step, handler }) {
     }
   };
 
+  const nameValidator = () => {
+    let validation =
+      /^[a-zA-Z\s]*$/.test(contactInfo.name) && contactInfo.name !== "";
+    validation ? setnameError(false) : setnameError(true);
+    return validation ? true : false;
+  };
+  const emailValidator = () => {
+    let validation =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        contactInfo.email.toString().toLowerCase()
+      );
+    validation === true ? setemailError(false) : setemailError(true);
+    return validation ? true : false;
+  };
+  const mobileNoValidator = () => {
+    let validation = /^(07)([0-9|7])(\d){7}$/.test(+contactInfo.mobileNumber);
+    validation === true
+      ? setmobileNumberError(false)
+      : setmobileNumberError(true);
+    return validation === true ? true : false;
+  };
+  const addressValidator = () => {
+    let validation =
+      /^[a-zA-Z\s]*$/.test(contactInfo.address) && contactInfo.address !== "";
+    validation === true ? setaddressError(false) : setaddressError(true);
+    return validation === true ? true : false;
+  };
+
+  const cityValidator = () => {
+    let validation =
+      /^[a-zA-Z\s]*$/.test(contactInfo.city) && contactInfo.city !== "";
+    validation === true ? setcityError(false) : setcityError(true);
+    return validation === true ? true : false;
+  };
+  const inputsValidator = () => {};
   const dateTimeHandler = (dateTime) => {
     let contactInfotemp = { ...contactInfo };
     contactInfotemp.time = dateTime;
@@ -51,47 +115,67 @@ function Contact({ devices, step, handler }) {
   };
 
   const submitHandler = () => {
-    console.log(contactInfo);
+    console.log(inputsValidator());
   };
+
   // JSX
   return (
     <div className="contact-form-container">
       <TextField
         required
+        error={nameError === true}
+        helperText={nameError ? "*Invalid Name (only use letters A-Z)" : ""}
         name="name"
         label="Name"
         type="text"
         onChange={inputHandler}
+        onBlur={nameValidator}
       />
       <TextField
         required
+        error={emailError === true}
+        helperText={emailError ? "Invalid Email Address" : ""}
         name="email"
         label="Email"
         type="email"
         onChange={inputHandler}
+        onBlur={emailValidator}
       />
       <TextField
         name="mobileNumber"
         label="Mobile Number"
+        error={mobileNumberError === true}
+        helperText={
+          mobileNumberError
+            ? "Invalid Mobile Number (use format:07XXXXXXXX)"
+            : ""
+        }
         type="tel"
         value={contactInfo.mobileNumber}
         onChange={inputHandler}
+        onBlur={mobileNoValidator}
         required
       />
       <TextField
         required
         name="address"
         label="Address"
+        error={addressError === true}
+        helperText={addressError ? "Address field can not be empty!" : ""}
         type="text"
         fullWidth
         onChange={inputHandler}
+        onBlur={addressValidator}
       />
       <TextField
         required
         name="city"
         label="City"
+        error={cityError === true}
+        helperText={cityError ? "City field can not be empty!" : ""}
         type="text"
         onChange={inputHandler}
+        onBlur={cityValidator}
       />
       <FormControl fullWidth>
         <FormLabel id="device-pickup-method-label">
